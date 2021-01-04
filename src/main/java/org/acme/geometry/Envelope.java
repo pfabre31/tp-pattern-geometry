a4b3914859e46e4b4fd5c0d2b7fd9527fad4aa2a
Paul Fabre <paulfabre31@gmail.com>
2021-01-04 17:00:24 +0100

    debut 0.6

---
+++ src/main/java/org/acme/geometry/Envelope.java
@@ -0,0 +1,42 @@
+package org.acme.geometry;
+
+public class Envelope {
+	
+	private Coordinate bottomLeft;
+	private Coordinate topRight;
+	
+	//Constructors
+	public Envelope() {
+		bottomLeft = new Coordinate();
+		topRight = new Coordinate();
+	};
+	
+	public Envelope(Coordinate bottomLeft, Coordinate topRight) {
+		this.bottomLeft = bottomLeft;
+		this.topRight = topRight;
+	};
+	
+	public boolean isEmpty() {
+		return this.bottomLeft.isEmpty();
+	};
+	
+	
+	//Getters
+	public double getXMin() {
+		return bottomLeft.getX();
+	};
+	
+	public double getYMin() {
+		return bottomLeft.getY();
+	};
+	
+	public double getXMax() {
+		return topRight.getX();
+	};
+	
+	public double getYMax() {
+		return topRight.getY();
+	};
+
+
+}
---
+++ src/main/java/org/acme/geometry/EnvelopeBuilder.java
@@ -0,0 +1,8 @@
+package org.acme.geometry;
+
+public class EnvelopeBuilder {
+	
+	public insert(Coordinate coordinate) {};
+	public Envelope build() {};
+
+}
